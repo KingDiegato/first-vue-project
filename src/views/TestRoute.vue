20713b8a238e8698d4d4353398c5380a9f375a03
Diego Gonzalez <gonzalezbusnego@gmail.com>
2023-03-31 14:12:22 -0400

    Start First vue Project

---
+++ src/views/TestRoute.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="try__route">
+    <h2>This is my route Try</h2>
+  </section>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .try__route {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
